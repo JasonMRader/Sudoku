a45238b87a6b23dc85802c678c17364d188eb79e
Jason rader <Jasonmrader@outlook.com>
2024-01-23 18:09:45 -0500

    Created a UItheme class to keep the color theme consistant

---
+++ Sudoku/UITheme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class UITheme
+    {
+        Color MainColor = Color.FromArgb(255, 255, 255);
+        Color AccentColor = Color.FromArgb(100, 130, 100);
+    }
+}
